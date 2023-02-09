df96d428c27052763f7ed8a2be9c86b05fffd8af
stefan__ <67799417+stefa-n@users.noreply.github.com>
2023-02-09 15:54:45 +0200

    Boop!

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,17 @@
 import Image from 'next/image';
 import Link from 'next/Link';
+
 export default function Navbar() {
     return (
         <div style={{backgroundColor: '#2a2a2a'}}>
-            <div style={{display: 'flex', alignItems: 'center'}}>
+            <div style={{display: 'flex', alignItems: 'center', fontFamily: "'Open Sans', sans-serif", fontWeight: '400'}}>
                 <Image src='/logo.png' alt='Store Logo' width={52} height={52} style={{marginLeft: '0.5em'}}/>
                 <Link href='/' style={{margin: '0px 10.5px 0px 10.5px'}}>
                     <p style={{color: '#ccc', fontSize: '0.75em'}}>STORE</p>
                 </Link>
+                <div style={{width: '1px', height: '30px', margin: '12px', backgroundColor: '#e7e7e7', opacity: '.55'}}/>
                 <Link href='/support' style={{margin: '0px 10.5px 0px 10.5px'}}>
                     <p style={{color: '#ccc', fontSize: '0.75em'}}>SUPPORT</p>
-                </Link>
-                <div style={{width: '100%'}}/>
-                <Link href='/cart' style={{backgroundColor: '#0078f2', margin: '0px', padding: '12px'}}>
-                    <p style={{color: '#ccc', fontSize: '0.75em'}}>CART</p>
                 </Link>
             </div>
         </div>
